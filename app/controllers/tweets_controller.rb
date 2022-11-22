c8d53104cb112b3a6768647a844b3c343a5c0311
Brendan McBride <brendan.mcmath@gmail.com>
2022-11-23 06:43:27 +0800

    add create tweet logic for rendering turbo stream

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,25 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all.order(created_at: :asc)
   end
 
-  def create; end
+  def create
+    @tweet = Tweeet.new(tweet_params)
+    respond_to do |format|
+      if @tweet.save
+        format.turbo_stream
+      else
+        format.html do
+          flash[:tweet_errors] = @tweet.errors.full_messages
+          redirect_to root_path
+        end
+      end
+    end
+  end
 
   def destroy; end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
+  end
 end
