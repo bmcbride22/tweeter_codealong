7eb3ffc6724a8c77b7cb2d2c16f84c095b65b2b0
Brendan McBride <brendan.mcmath@gmail.com>
2022-11-23 06:34:32 +0800

    initial tweet style

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,10 @@
 class TweetsController < ApplicationController
   def index
+    @tweet = Tweet.new
+    @tweets = Tweet.all.order(created_at: :asc)
   end
 
-  def create
-  end
+  def create; end
 
-  def destroy
-  end
+  def destroy; end
 end
