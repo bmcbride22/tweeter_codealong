96e16f198fbd8c70d01c115353b39d3f6c910d15
Brendan McBride <brendan.mcmath@gmail.com>
2022-11-23 13:16:13 +0800

    Add username to user model and add username validation to registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tweets
+
+  validates_uniqueness_of :username
+  validates_presence_of :username
 end
