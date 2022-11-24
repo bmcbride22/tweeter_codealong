35ad9935b6f20384fa7809863e889fef17017fa6
Brendan McBride <brendan.mcmath@gmail.com>
2022-11-24 20:53:16 +0800

    add retweeted tweets to tweet feed and wire up retweet button

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -22,14 +22,31 @@ class TweetsController < ApplicationController
     end
   end
 
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
   def destroy
     @tweet = current_user.tweets.find(params[:id])
     @tweet.destroy
   end
 
+  def retweet
+    @tweet = Tweet.find(params[:id])
+    retweet = current_user.tweets.new(tweet_id: @tweet.id)
+
+    respond_to do |format|
+      if retweet.save
+        format.turbo.save
+      else
+        format.html { redirect_back fallback_location: @tweet, alert: 'Could not retweet' }
+      end
+    end
+  end
+
   private
 
   def tweet_params
-    params.require(:tweet).permit(:body)
+    params.require(:tweet).permit(:body, :tweet_id)
   end
 end
