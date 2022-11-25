7b09acd5c804e3a1cd9f27349a7fe5519a5787b2
Brendan McBride <brendan.mcmath@gmail.com>
2022-11-25 12:13:58 +0800

    add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,32 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_tweet
 
-  def create; end
+  def create
+    @comment = @tweet.comments.new(comment_params.merge(user: current_user))
+    respond_to do |format|
+      if @comment.save
+        format.turbo_stream
+      else
+        format.html do
+          redirect_to tweet_path(@tweet), alert: 'Comment could not be created'
+        end
+      end
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @comment = @tweet.comments.find(params[:id])
+    @comment.destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:tweet_id])
+  end
 end
